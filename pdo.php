3f614f7137d7a1f7998ac57b143a86220d5d73c0
Heidi <heidijoyguenther@gmail.com>
2019-02-04 12:23:05 -0700

    removed php end tag

--- pdo.php
+++ pdo.php
@@ -2,4 +2,3 @@
 $pdo = NEW PDO('mysql:host=localhost;port=3306;dbname=misc', 'name', 'password');
 // see 'errors' folder for details
 $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-?>
